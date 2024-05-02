616e5b9df6fc9d32da2e9606066332f3a360a208
malldonado <matheussilvacontat@gmail.com>
2024-05-02 18:07:24 -0300

    update project

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,24 +1,26 @@
-import { useState } from "react";
-import { Link } from "react-router-dom";
-import axios from 'axios';
+import {Link, Navigate} from "react-router-dom";
+import {useContext, useState} from "react";
+import axios from "axios";
+import {UserContext} from "../UserContext.jsx";
 
 export default function LoginPage() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   const [redirect, setRedirect] = useState(false);
-
+  const {setUser} = useContext(UserContext);
   async function handleLoginSubmit(ev) {
     ev.preventDefault();
     try {
-      await axios.post('/login', {email, password});
+      const {data} = await axios.post('/login', {email,password});
+      setUser(data);
       alert('Login successful');
       setRedirect(true);
-    }catch(e) {
+    } catch (e) {
       alert('Login failed');
     }
   }
 
-  if(redirect) {
+  if (redirect) {
     return <Navigate to={'/'} />
   }
 
@@ -27,24 +29,17 @@ export default function LoginPage() {
       <div className="mb-64">
         <h1 className="text-4xl text-center mb-4">Login</h1>
         <form className="max-w-md mx-auto" onSubmit={handleLoginSubmit}>
-          <input
-            type="email"
-            placeholder="your@email.com"
-            value={email}
-            onChange={(ev) => setEmail(ev.target.value)}
-          />
-          <input
-            type="password"
-            placeholder="password"
-            value={password}
-            onChange={(ev) => setPassword(ev.target.value)}
-          />
+          <input type="email"
+                 placeholder="your@email.com"
+                 value={email}
+                 onChange={ev => setEmail(ev.target.value)} />
+          <input type="password"
+                 placeholder="password"
+                 value={password}
+                 onChange={ev => setPassword(ev.target.value)} />
           <button className="primary">Login</button>
           <div className="text-center py-2 text-gray-500">
-            Don't have an account yet?
-            <Link className="underline text-black ml-1" to={"/register"}>
-              Register
-            </Link>
+            Don't have an account yet? <Link className="underline text-black" to={'/register'}>Register now</Link>
           </div>
         </form>
       </div>
